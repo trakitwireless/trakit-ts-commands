77b246ab69a7302f6526cb3e866365a6a16a706b
Alex Lein <alein@trakitwireless.com>
2025-03-20 10:52:29 -0400

    Let's get this party started

---
+++ commands.ts
@@ -0,0 +1,41 @@
+﻿/**
+ * Object definitions that work with our various APIs.
+ * {@link https://github.com/trakitwireless/trakit-ts-commands|TypeScript documentation.}
+ * Last updated on Thu Feb 27 2025 11:59:01 
+ * @copyright Trak-iT Wireless Inc. 2025
+ */
+
+
+/**
+ * Version number for this release.
+ */
+const version = 5.0;
+
+export default {
+    version,
+
+    //#region Company
+    //#endregion Company
+    //#region Accounts
+    //#endregion Accounts
+    //#region Assets
+    //#endregion Assets
+    //#region Behaviours
+    //#endregion Behaviours
+    //#region Billing
+    //#endregion Billing
+    //#region Dispatch
+    //#endregion Dispatch
+    //#region Hosting
+    //#endregion Hosting
+    //#region Images
+    //#endregion Images
+    //#region Maintenance
+    //#endregion Maintenance
+    //#region Places
+    //#endregion Places
+    //#region Providers
+    //#endregion Providers
+    //#region Reports
+    //#endregion Reports
+};
---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import typescript from 'rollup-plugin-typescript2';
+import terser from '@rollup/plugin-terser';
+
+const obfuscate = {
+  ecma: 2016,
+  // compress: {
+  //   drop_console: true,
+  //   drop_debugger: true,
+  //   hoist_funs: true,
+  //   join_vars: true,
+  //   module: true,
+  //   toplevel: true,
+  // },
+  sourceMap: {
+    filename: '../_publish/trakit-commands.min.js',
+  },
+};
+
+export default [
+  {
+    input: 'commands.ts',
+    output: [
+      // {
+      //   file: '../_publish/trakit-commands.js',
+      //   format: 'es',
+      // },
+      {
+        file: '../_publish/trakit-commands.min.js',
+        format: 'es',
+        plugins: [terser(obfuscate)]
+      }
+    ],
+    plugins: [typescript()],
+  }
+];
