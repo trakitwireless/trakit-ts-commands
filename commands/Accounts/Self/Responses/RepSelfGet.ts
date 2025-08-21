87dd0d968852b38377d20c354c10e80ac55f6aa8
Alex Lein <alein@trakitwireless.com>
2025-08-20 23:24:22 -0400

    self get details are sometimes not present, like if you are not logged in

--- commands/Accounts/Self/Responses/RepSelfGet.ts
+++ commands/Accounts/Self/Responses/RepSelfGet.ts
@@ -1,4 +1,8 @@
 import { Reply } from "../../../API/Responses/Reply";
+import { SelfMachine } from "./Content/SelfMachine";
+import { SelfUser } from "./Content/SelfUser";
+import { SessionPolicy } from "@objects/Companies/SessionPolicy";
+import { PasswordPolicy } from "@objects/Companies/PasswordPolicy";
 
 /**
  * A container for the <see cref="User"/> or <see cref="Machine"/> of the current session.
@@ -7,7 +11,7 @@ export class RepSelfGet extends Reply {
 	/**
 	 * Your session identifier.
 	 **/
-	ghostId: string;
+	ghostId: string | undefined;
 	/**
 	 * The timestamp of when this session expires.
 	 **/
@@ -16,21 +20,22 @@ export class RepSelfGet extends Reply {
 	 * This session's <see cref="User"/> details (if the service is being used by a <see cref="User"/>).
 	 * If this value is not present, then the session is not yet authenticated.
 	 **/
-	user: SelfUser;
+	user: SelfUser | undefined;
 	/**
 	 * This <see cref="Machine"/>'s details (if the service is being used by a <see cref="Machine"/>).
 	 * If this value is not present, then the session is not a machine account.
 	 **/
-	machine: SelfMachine;
+	machine: SelfMachine | undefined;
 	/**
 	 * This <see cref="User"/>'s <see cref="CompanyPolicies.sessionPolicy"/>.
 	 **/
-	sessionPolicy: SessionPolicy;
+	sessionPolicy: SessionPolicy | undefined;
 	/**
 	 * This <see cref="User"/>'s <see cref="CompanyPolicies.passwordPolicy"/>.
 	 **/
-	passwordPolicy: PasswordPolicy;
+	passwordPolicy: PasswordPolicy | undefined;
 	/**
 	 * The UTC date/time of the server hosting the connection.
 	 **/
-	serverTime: Date;}
+	serverTime!: Date;
+}
