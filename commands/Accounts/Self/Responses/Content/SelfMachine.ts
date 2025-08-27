80983bf860dbf24d197fd4ea76bec099c52f16ff
Alex Lein <alein@trakitwireless.com>
2025-08-27 18:24:59 -0400

    well, at least this builds

--- commands/Accounts/Self/Responses/Content/SelfMachine.ts
+++ commands/Accounts/Self/Responses/Content/SelfMachine.ts
@@ -1,11 +1,34 @@
-import { Machine } from "@objects/Accounts/Machine";
+import trakit from "@trakit/objects";
 
 /**
  * A container for the details of the {@link Machine} requested.
  **/
-export class SelfMachine extends Machine {
-	/**
-	 * A list of groups to which this machine account belongs.
-	 * @see {@link UserGroup.id}
-	 **/
-		new public UserGroup[] groups;}
+export class SelfMachine extends trakit.Machine {
+	constructor(json?: any | null) {
+		super(json);
+	}
+
+	override fromJSON(json: any, force?: boolean): boolean {
+		let update: boolean = false;
+		if (json) {
+			const groups = json["groups"] as any[],
+				contact = json["contact"] as any;
+			json["contact"] = contact?.id ?? null;
+			json["groups"] = groups.map(g => g.id);
+			update = super.fromJSON(json);
+			if (update) {
+				if (contact) {
+					let cont = trakit.storage.contacts.get(contact.id);
+					if (!cont) trakit.storage.contacts.set(contact.id, new Contact);
+					cont?.fromJSON(contact);
+				}
+				for (let obj of groups) {
+					let group = trakit.storage.userGroups.get(obj.id);
+					if (!group) trakit.storage.userGroups.set(obj.id, new UserGroup);
+					group?.fromJSON(obj);
+				}
+			}
+		}
+		return update;
+	}
+}
