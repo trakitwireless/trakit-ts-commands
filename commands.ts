066b099b40eee9367f0bd1aef1e63c901a8b6bcf
Alex Lein <alein@trakitwireless.com>
2025-08-27 20:49:00 -0400

    Setting up initial exports

--- commands.ts
+++ commands.ts
@@ -1,45 +1,68 @@
 ﻿/**
- * Trak-iT API Commands Definition
- * {@link https://github.com/trakitwireless/trakit-ts-commands|Commands definition.}
- * The Trak-iT APIs use the same command definitions for all HTTPS, RESTful, and WebSocket commands.
- * For example, a "get icon" command in the RESTful service will return a JSON definition of the icon,
- * whereas the same command to the Imaging service will return a PNG.
+ * Object definitions that work with our various APIs.
+ * {@link https://github.com/trakitwireless/trakit-ts-commands|TypeScript documentation.}
  * Last updated on Thu Feb 27 2025 11:59:01 
  * @copyright Trak-iT Wireless Inc. 2025
- **/
+ */
+import { TrakitCommander } from "clients/TrakitCommander";
+import { ErrorCode } from "./commands/API/Responses/Errors/ErrorCode";
+import { TrakitObjectCommander } from "./clients/TrakitObjectCommander";
+
 
 /**
  * Version number for this release.
  */
-const version = 5.02;
+export const version = 5.03;
 
-export default {
-    version,
-    //#region Utility, conversion, and encoding functions
-    //#endregion Utility, conversion, and encoding functions
+/**
+ * Command execution classes.
+ */
+export {
+    TrakitCommander,
+    TrakitObjectCommander,
+};
+    
+/**
+ * Command request payload and response content classes.
+ */
+export {
+    //#region API
+    ErrorCode,
+    //#endregion API
+
+    //#region Accounts
+    //#endregion Accounts
+    
+    //#region Assets
+    //#endregion Assets
+
+    //#region Behaviours
+    //#endregion Behaviours
+
+    //#region Billing
+    //#endregion Billing
 
     //#region Company
     //#endregion Company
-    //#region Accounts
-    //#endregion Accounts
-    //#region Assets
-    //#endregion Assets
-    //#region Behaviours
-    //#endregion Behaviours
-    //#region Billing
-    //#endregion Billing
+
     //#region Dispatch
     //#endregion Dispatch
+
     //#region Hosting
     //#endregion Hosting
+    
     //#region Images
     //#endregion Images
+
     //#region Maintenance
     //#endregion Maintenance
+
     //#region Places
     //#endregion Places
+
     //#region Providers
     //#endregion Providers
+
     //#region Reports
     //#endregion Reports
-}
+};
