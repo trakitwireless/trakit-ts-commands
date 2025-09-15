71bd97a996b2f8462e1cf3ccb95f8344148e03ae
Alex Lein <alein@trakitwireless.com>
2025-09-15 14:26:30 -0400

    Starting on hosted

--- commands/Hosting/Documents/Requests/Parameters/ParamDocumentMerge.ts
+++ commands/Hosting/Documents/Requests/Parameters/ParamDocumentMerge.ts
@@ -1,3 +1,4 @@
+import { nothing, serialization, ulong, utility } from "@trakit/objects";
 import { ParamMergeSubscribable } from "../../../../API/Requests/Parameters/ParamMergeSubscribable";
 
 /**
@@ -8,66 +9,56 @@ export class ParamDocumentMerge extends ParamMergeSubscribable {
 	 * The unique identifier of the {@link Document} you want to update.
 	 * Leave this as `null` when creating a new {@link Document}.
 	 **/
-	id: ulong | undefined;
+	id: ulong | nothing;
 	/**
 	 * The {@link Company} to which this {@link Document} belongs.
 	 * After creation, this value is read-only.
 	 **/
-	company: ulong | undefined;
+	company: ulong | nothing;
 	/**
 	 * Name for the {@link Document}.
 	 **/
-	name: string;
+	name: string | nothing;
 	/**
 	 * Notes for the {@link Document}.
 	 **/
-	notes: string;
+	notes: string | nothing;
 	/**
-	 * A collection of other names this person might go by.
-	 * Use the object key like a name identifier.
-	 * Example keys: Initials, Nickname, Maiden Name, etc.
+	 * The time at which the {@link Document} will automatically be purged from the system.
 	 **/
-	otherNames: Map<string, string>;
+	expiry: Date | nothing;
 	/**
-	 * Email addresses
-	 * Use the object key like a name of the address.
-	 * Example keys: Home, Work, Support, Old, etc.
+	 * Name/value collections of custom fields used to refer to external systems.
+	 * If the value is null, the references are removed from the {@link Document}.
 	 **/
-	emails: Map<string, string>;
-	/**
-	 * Phone numbers.
-	 * Use the object key like a name of the phone number.
-	 * Example keys: Mobile, Fax, Home, Office, etc.
-	 **/
-	phones: Map<string, ulong?>;
-	/**
-	 * Mailing addresses
-	 * Use the object key like a name of the address.
-	 * Example keys: Home, Work, Park, etc.
-	 **/
-	addresses: Map<string, string>;
-	/**
-	 * Websites and other online resources
-	 * Use the object key like a name of the address.
-	 * Example keys: Downloads, Support, FTP, etc.
-	 **/
-	urls: Map<string, Uri>;
-	/**
-	 * Date information
-	 * Use the object key like a name of the date.
-	 * Example keys: Birthday, Started Date, Retired On, etc.
-	 **/
-	dates: Map<string, Date?>;
-	/**
-	 * Uncategorized information
-	 * Use the object keys and values however you'd like.
-	 **/
-	options: Map<string, string>;
-	/**
-	 * A list of roles they play in the {@link Company}.
-	 **/
-	roles: string[];
-	/**
-	 * {@link Picture}s of this {@link Document}.
-	 **/
-	pictures: ulong[];}
+	references: Map<string, string | nothing> | nothing;
+
+	constructor(json: any) {
+		super(json);
+		this.id = json?.id;
+		this.company = json?.company;
+		this.name = json?.name;
+		this.notes = json?.notes;
+		this.expiry = json?.expiry
+			? utility.date(json.expiry)
+			: null;
+		this.references = json?.references
+			? serialization.toMap(json.references)
+			: null;
+	}
+
+	override toJSON(): any {
+		const json: any = {};
+		if (this.id) {
+			json.id = this.id;
+			json.v = [...this.v];
+		} else {
+			json.company = this.company;
+		}
+		if (this.name) json.name = this.name;
+		if (this.notes) json.notes = this.notes;
+		if (this.expiry?.valueOf()) json.expiry = this.expiry.toISOString();
+		if (this.references?.size) json.references = serialization.fromMap(this.references);
+		return json;
+	}
+}
