b390f0c5789042d768e97f47faf4a6e8a3049687
Alex Lein <alein@trakitwireless.com>
2025-09-23 13:17:45 -0400

    Payload class updated with fixed splitter regex Made it abstract because I don't have a better way of figuring out how to deserialize the responses in the sync lib

--- commands/API/Requests/Payload.ts
+++ commands/API/Requests/Payload.ts
@@ -1,7 +1,8 @@
 import { int } from '@trakit/objects';
+import { Reply } from '../Responses/Reply';
 
-/// Used to split the Payload class name into pieces to help create commands
-const Payload_SPLITTER = /Pay([A-Z][a-z]+)+?((?:Batch)?(?:Get|List|Merge|Delete|Restore|Suspend|Revive|Cancel|Change))(By.+)?/;
+// Used to split the Payload class name into pieces to help create commands
+const Payload_SPLITTER = /Pay((?:[A-Z][a-z]+)+?)(Batch)?(Get|List|Merge|Delete|Restore|Suspend|Revive|Cancel|Change)(?:By(.+))?/;
 
 /**
  * Base class for all command parameters.
@@ -11,20 +12,45 @@ const Payload_SPLITTER = /Pay([A-Z][a-z]+)+?((?:Batch)?(?:Get|List|Merge|Delete|
  * Child classes should contain members required to execute a command.
  * </remarks>
  **/
-export class Payload {
-	/**
-	 * Splits this class' name into parts helpful to {@link TrakitCommander{TClient}.Command{TReply}(Payload)}.
-	 **/
-	public getNameParts(): string[] {
-		return [...this.constructor.name.matchAll(Payload_SPLITTER)].map(m => m[1] ?? "");
-	}
-
+export abstract class Payload {
 	/**
 	 * Identifier used by external system to correlate requests to responses.
-	 **/
+	 */
 	reqId: int | undefined;
 
 	constructor(json?: any) {
 		this.reqId = json?.reqId;
 	}
+
+	/**
+	 * Splits this class' name into parts helpful to breaking down how the command request should be sent.
+	 * The returned array contains:
+	 * - [0]: Name of the object being acted upon (ie: `Asset`, `ProviderGeneral`, `User`, etc.)
+	 * - [1]: A flag for batch operations. This will be `Batch` if the operation is a batch operation, or an empty string otherwise.
+	 * - [2]: The action being performed (ie: `Get`, `List`, `Merge`, `Delete`, etc.)
+	 * - [3]: The filter type, if any (ie: `Asset`, `Company`, `CompanyAndLabels`, etc.). This will be an empty string if no filter is used.
+	 */
+	getNameParts(): [
+		string,
+		"" | "Batch",
+		"Get" | "List" | "Merge" | "Delete" | "Restore" | "Suspend" | "Revive" | "Cancel" | "Change",
+		string
+	] {
+		return [...this.constructor.name.match(Payload_SPLITTER) as string[]]
+			.slice(1)
+			.map(m => m ?? "") as [
+				string,
+				"" | "Batch",
+				"Get" | "List" | "Merge" | "Delete" | "Restore" | "Suspend" | "Revive" | "Cancel" | "Change",
+				string
+			];
+	}
+
+	/**
+	 * Creates a reply object from the given JSON data.
+	 * Each Payload will have a corresponding Reply type.
+	 * @param json 
+	 * @returns 
+	 */
+	abstract createReply(json: any): Reply;
 }
