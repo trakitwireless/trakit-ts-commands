5c708e9f36e2b439ac26659d860b31c64406283b
Alex Lein <alein@trakitwireless.com>
2025-10-28 17:28:10 -0400

    This should be a prototype that works for doing the sync to storage

--- commands/API/Responses/ReplyGet.ts
+++ commands/API/Responses/ReplyGet.ts
@@ -1,27 +1,24 @@
-import { Base, classes, Contact, IDeserializable, IRequestable, ISerializable, JsonObject, nothing, storage, ulong, utility } from '@trakit/objects';
-import { Reply } from './Reply';
+import { IDeserializable, IRequestable, JsonObject, storage, utility } from '@trakit/objects';
+import { ReplyBaseGetter } from './ReplyBaseGetter';
 
 /**
  * Base class for all responses from commands.
  * All command response classes use this as the base.
  **/
-export abstract class ReplyGet<TRequestable extends IRequestable> extends Reply {
-
-	protected _json: JsonObject | nothing;
-	abstract getTypeName(): classes;
-	abstract getObject(): TRequestable;
+export abstract class ReplyGet<TRequestable extends IRequestable> extends ReplyBaseGetter<JsonObject> {
 
 	constructor(json: JsonObject) {
 		super(json);
-		this._json = json?.[utility.capitalize(this.getTypeName())] as JsonObject;
+		this._json = json?.[utility.capitalize(this._getTypeName(), false)] as JsonObject;
 	}
 
-	store(): void {
-		const map = storage[this.getTypeName()],
+	abstract getObject(): TRequestable;
+	override store(): void {
+		const map = storage[this._getTypeName()],
 			obj = this.getObject(),
 			key = obj.getKey(),
 			stored = map.get(key) as unknown as IDeserializable;
 		if (!stored) map.set(key, obj);
-		else stored.fromJSON(this._json as JsonObject);
+		else stored.fromJSON(this._json);
 	}
 }
---
+++ commands/API/Responses/ReplyList.ts
@@ -0,0 +1,28 @@
+import { IDeserializable, IRequestable, JsonObject, storage, utility } from '@trakit/objects';
+import { ReplyBaseGetter } from './ReplyBaseGetter';
+
+/**
+ * Base class for all responses from commands.
+ * All command response classes use this as the base.
+ **/
+export abstract class ReplyList<TRequestable extends IRequestable> extends ReplyBaseGetter<JsonObject[]> {
+
+	constructor(json: JsonObject) {
+		super(json);
+		this._json = json?.[utility.plural(utility.capitalize(this._getTypeName(), false))] as JsonObject[];
+	}
+
+	abstract getCollection(): TRequestable[];
+	override store(): void {
+		const map = storage[this._getTypeName()],
+			collection = this.getCollection();
+		for (let i = 0; i < collection.length; i++) {
+			const json = this._json[i],
+				item = collection[i],
+				key = item.getKey(),
+				stored = map.get(key) as unknown as IDeserializable;
+			if (!stored) map.set(key, item);
+			else stored.fromJSON(json);
+		}
+	}
+}
