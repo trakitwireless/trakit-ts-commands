c1f3a593a31f165d4b48652a44902f322a39fa06
Alex Lein <alein@trakitwireless.com>
2025-10-30 18:09:40 -0400

    Clearing behaviour logs is slightly diffferent from other batch deletes

--- commands/Behaviours/Logs/Responses/RepBehaviourLogBatchDelete.ts
+++ commands/Behaviours/Logs/Responses/RepBehaviourLogBatchDelete.ts
@@ -1,46 +1,69 @@
-import { JsonObject, nothing } from "@trakit/objects";
+import { BehaviourLog, JsonObject, nothing, storage, ulong } from "@trakit/objects";
 import { ContentId } from "../../../API/Responses/Content/ContentId";
 import { ReplySyncBatchDelete } from "../../../API/Responses/ReplySyncBatchDelete";
 
 /**
+ * 
+ */
+abstract class RepBehaviourLogBatchDelete extends ReplySyncBatchDelete {
+	/**
+	 * A filter function to select which {@link BehaviourLog}s to delete from storage.
+	 */
+	protected _filter: (log: BehaviourLog) => boolean;
+
+	constructor(json: JsonObject, filter: (log: BehaviourLog) => boolean) {
+		super(json, "BehaviourLog");
+		this._filter = filter;
+	}
+	protected override _getKeys() {
+		return [
+			...(storage[this._typeName] as Map<ulong, BehaviourLog>)
+				.entries()
+				.filter(([key, log]) => this._filter(log))
+				.map(([key, log]) => key)
+		];
+	}
+}
+
+/**
  * A container for the {@link behaviourLog}.
  **/
-export class RepBehaviourLogBatchDeleteByAsset extends Reply {
+export class RepBehaviourLogBatchDeleteByAsset extends RepBehaviourLogBatchDelete {
 	/**
 	 * Identifier of the {@link Asset} to which this collection belongs.
 	 **/
 	asset: ContentId | nothing;
 
 	constructor(json: JsonObject) {
-		super(json);
+		super(json, (log) => log.assetId === this.asset?.id);
 		this.asset = ContentId.fromJSON(json?.asset as JsonObject);
 	}
 }
 /**
  * A container for the {@link behaviourLog}.
  **/
-export class RepBehaviourLogBatchDeleteByBehaviour extends Reply {
+export class RepBehaviourLogBatchDeleteByBehaviour extends RepBehaviourLogBatchDelete {
 	/**
 	 * Identifier of the {@link Behaviour} to which this collection belongs.
 	 **/
 	behaviour: ContentId | nothing;
 
 	constructor(json: JsonObject) {
-		super(json);
+		super(json, (log) => log.behaviourId === this.behaviour?.id);
 		this.behaviour = ContentId.fromJSON(json?.behaviour as JsonObject);
 	}
 }
 /**
  * A container for the {@link behaviourLog}.
  **/
-export class RepBehaviourLogBatchDeleteByScript extends Reply {
+export class RepBehaviourLogBatchDeleteByScript extends RepBehaviourLogBatchDelete {
 	/**
 	 * Identifier of the {@link BehaviourScript} to which this collection belongs.
 	 **/
 	behaviourScript: ContentId | nothing;
 
 	constructor(json: JsonObject) {
-		super(json);
+		super(json, (log) => log.behaviour?.scriptId === this.behaviourScript?.id);
 		this.behaviourScript = ContentId.fromJSON(json?.behaviourScript as JsonObject);
 	}
 }
