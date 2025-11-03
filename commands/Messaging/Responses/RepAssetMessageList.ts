5d50f91d81571cf2dfa84489ab951f8fb4cd2e2a
Alex Lein <alein@trakitwireless.com>
2025-11-03 12:34:16 -0500

    Places and messages

--- commands/Messaging/Responses/RepAssetMessageList.ts
+++ commands/Messaging/Responses/RepAssetMessageList.ts
@@ -1,4 +1,4 @@
-import { AssetMessage, JsonObject, nothing } from "@trakit/objects";
+import { AssetMessage, email, guid, JsonObject, nothing, ulong } from "@trakit/objects";
 import { ContentId } from "../../API/Responses/Content/ContentId";
 import { IRepListByAsset } from "../../API/Responses/IRepListByAsset";
 import { IRepListByCompany } from "../../API/Responses/IRepListByCompany";
@@ -33,6 +33,9 @@ export class RepAssetMessageListByCompany extends RepAssetMessageList implements
 		super(json);
 		this.company = ContentId.fromJSON(json?.company as JsonObject);
 	}
+	override _filterCollection(pair: [string | guid | email | ulong, AssetMessage], index: number): boolean {
+		return pair[1].companyId === (this.company as ContentId).id;
+	}
 }
 /**
  * Contains the {@link Asset.id} of the collection.
@@ -46,5 +49,8 @@ export class RepAssetMessageListByAsset extends RepAssetMessageList implements I
 	constructor(json: JsonObject) {
 		super(json);
 		this.asset = ContentId.fromJSON(json?.asset as JsonObject);
+	}
+	override _filterCollection(pair: [string | guid | email | ulong, AssetMessage], index: number): boolean {
+		return pair[1].assetId === (this.asset as ContentId).id;
 	}
 }
--- commands/Places/Responses/RepPlaceList.ts
+++ commands/Places/Responses/RepPlaceList.ts
@@ -1,4 +1,4 @@
-import { JsonObject, nothing, Place } from "@trakit/objects";
+import { email, guid, JsonObject, nothing, Place, ulong } from "@trakit/objects";
 import { ContentId } from "../../API/Responses/Content/ContentId";
 import { ReplySyncList } from "../../API/Responses/ReplySyncList";
 
@@ -30,5 +30,8 @@ export class RepPlaceListByCompany extends RepPlaceList {
 	constructor(json: JsonObject) {
 		super(json);
 		this.company = ContentId.fromJSON(json?.company as JsonObject);
+	}
+	override _filterCollection(pair: [string | guid | email | ulong, Place], index: number): boolean {
+		return pair[1].companyId === (this.company as ContentId).id;
 	}
 }
