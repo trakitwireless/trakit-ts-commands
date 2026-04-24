2d72276e8610426dca3275bac66807e290f59211
Alex Lein <alein@trakitwireless.com>
2026-04-24 15:57:30 -0400

    One more publish

--- commands/index.ts
+++ commands/index.ts
@@ -585,7 +585,7 @@ import { RepSubscriptionList } from "./WebSocket/Responses/RepSubscriptionList";
 /**
  * Version number for this release.
  */
-export const version = '0.0.28';
+export const version = '0.0.29';
 
 //#region API
 /**
