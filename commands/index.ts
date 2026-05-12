f26d151723a8ce81007ccaa89ce7996a545ce012
Alex Lein <alein@trakitwireless.com>
2026-05-12 15:46:49 -0400

    Updated objects for rebuild

--- commands/index.ts
+++ commands/index.ts
@@ -585,7 +585,7 @@ import { RepSubscriptionList } from "./WebSocket/Responses/RepSubscriptionList";
 /**
  * Version number for this release.
  */
-export const version = '0.0.35';
+export const version = '0.0.36';
 
 //#region API
 /**
