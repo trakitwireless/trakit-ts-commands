45f74a2867a5a61c9639c6bf09bceb36ce3f76b5
Alex Lein <alein@trakitwireless.com>
2026-07-14 12:52:43 -0400

    Unused

--- src/Assets/Requests/PayAssetAdvancedAudit.ts
+++ src/Assets/Requests/PayAssetAdvancedAudit.ts
@@ -2,7 +2,6 @@ import { AssetAdvanced, JsonObject, SyncName } from "@trakit/objects";
 import { IPayListByAsset } from "../../API/Requests/IPayListByAsset";
 import { ParamId } from "../../API/Requests/Parameters/ParamId";
 import { PayloadAudit } from "../../API/Requests/PayloadAudit";
-import { ActionType } from "../../API/Requests/Payload";
 import { Reply } from "../../API/Responses/Reply";
 import { RepAssetAdvancedAudit } from "../Responses/RepAssetAdvancedAudit";
 
